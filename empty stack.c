3e1b86ff250b6612e857e168ab6d8568c673215b
kristina-wu <812315376@qq.com>
2017-05-29 15:20:22 +0800

    Create empty stack.c

---
+++ empty stack.c
@@ -0,0 +1,12 @@
+#include>=<stdio.h>
+#include"include/empty stack.h"
+int isempty(linkstack h)  // 判断栈是否为空
+{
+    if(h->next==NULL)
+    {
+        printf("is empty\n");
+        return 1;
+    }
+    return 0;
+
+}
