b05786d1d2a3ee70139684f3de6a2dfd42465bb3
kristina-wu <812315376@qq.com>
2017-05-29 15:15:50 +0800

    Create enqueue.c

---
+++ enqueue.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include"include/enquene.h"
+int EnQueue(sequeue_t *queue, data_t x)  //入队
+{  
+    if (NULL == queue) return - 1;  
+  
+    if (1 == FullSequeue(queue)) return -1; /* full */  
+  
+    queue->rear = (queue->rear + 1) % N;  
+    queue->data[queue->rear] = x;  
+  
+    return 0;  
+}  
